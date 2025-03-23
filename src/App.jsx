cd63532a8e698dc65e64d2a677b6ca733a388c57
mehdi rezaei arshad <mehdirezaeiarshad@gmail.com>
2025-03-23 21:17:51 +0330

    just a little bit more

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,64 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import React from 'react'
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import '/src/css.files/App.css';
 import { ScreenSizeProvider, useScreenSize } from './contexts';
+import { DarkThemeContext, DarkThemeContextProvider } from './contexts';
+import { LightThemeContext, LightThemeContextProvider } from './contexts';
+import { EnglishLanguageContext,  EnglishLanguageContextProvider} from './contexts';
 
+const TypingAnimation = (fullText) => {
+  const[displayText, setDisplayText] = useState("");
+  const typingSpeed = 100;
+  useEffect(() => {
+    let currentIndex = 0;
+
+    const typeText = () => {
+      if (currentIndex < fullText.lenght) {
+        setDisplayText((prevText) => prevText + fullText[currentIndex]);
+        currentIndex++;
+        setTimeout(typeText, typingSpeed)
+      }
+    }
+    typeText();
+    return () => clearTimeout(typeText)
+  })
+  return {displayText}
+}
 
 function TitlePart() {
   const {isMobile} = useScreenSize()
+  const {title} = useContext(EnglishLanguageContext)
   return (
     <>
-    {isMobile ? <div>
-      <img className="profile-pic fadein" src="https://s6.uupload.ir/files/msg23372500-17253_e56h.jpg" />
-      <h1>Hi, I'm Mehdi</h1>
+    {isMobile ? <div className="row">
+      {/* this is for mobile */}
+      <img className="profile-pic d-block mx-auto img-fluid fadein col-6" src="public\page-pictures\dark-theme\about-me\altumcode-U0tBTn8UR8I-unsplash.jpg" />
+      <h1>Hi</h1>
+      <h1>I'm Mehdi</h1>
       <button>plus</button>
-    </div> : <div>
-        <img className="profile-pic fadein" src="https://s6.uupload.ir/files/msg23372500-17253_e56h.jpg"/>
-        <h1>Hi, I'm mehdi</h1>
+    </div> : <div style={{width: "100%"}} className="d-flex row">
+      {/* this for laptop */}
+        <div style={{textAlign: "right"}} className="col-5 mt-auto mb-auto ms-auto p-0">
+          <h1 className="fade-in ms-auto"><span className="highlight">FRONTEND</span> DEVELOPER</h1>
+          <p className='col-7 ms-auto' dangerouslySetInnerHTML={{__html: title.firstPart }} />
+        </div>
+        <div className="col-7 p-0">
+          <img className="profile-pic img-fluid me-auto fadein" src="public\page-pictures\dark-theme\about-me\altumcode-U0tBTn8UR8I-unsplash.jpg"/>
+        </div>
       </div>}
     </>
   )
 }
+
 function NavBar() {
+
   return (
-    <div></div>
+    <>
+    </>
   )
 }
+
 function DashBoard() {
   const {isMobile} = useScreenSize()
   const [isLoading, setIsLoading] = useState(true);
@@ -36,7 +71,8 @@ function DashBoard() {
     <>
     {isLoading ? <div className="loader"></div> :
       <>
-      <TitlePart />
+        <TitlePart />
+        
       <NavBar />
       </>
     }
@@ -46,9 +82,15 @@ function DashBoard() {
 function App() {
   return (
     <>
-      <ScreenSizeProvider>
-        <DashBoard />
-      </ScreenSizeProvider>
+      <EnglishLanguageContextProvider>
+        <LightThemeContextProvider>
+          <DarkThemeContextProvider>
+            <ScreenSizeProvider>
+              <DashBoard />
+            </ScreenSizeProvider>
+          </DarkThemeContextProvider>
+        </LightThemeContextProvider>  
+      </EnglishLanguageContextProvider>
     </>
   )
 }
--- src/contexts.jsx
+++ src/contexts.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext } from 'react';
+import React, { createContext, useContext, useState } from 'react';
 import { useMediaQuery } from 'react-responsive';
 
 const ScreenSizeContext = createContext();
@@ -12,5 +12,73 @@ export const ScreenSizeProvider = ({ children }) => {
         <ScreenSizeContext.Provider value={{isMobile}}>
             {children}
         </ScreenSizeContext.Provider>
+    )
+}
+
+export const ThemeContext = createContext({
+    theme: "dark",
+    setTheme: () => {}
+});
+
+export const ThemeContextProvider = ({ children }) => {
+    const [theme, setTheme] = useState("dark")
+    return (
+        <ThemeContext.Provider value={{theme, setTheme}}>
+            {children}
+        </ThemeContext.Provider>
+    )
+
+}
+
+export const DarkThemeContext = createContext({
+    backgroundColor: "#151515",
+    forgroundColor: "gray",
+    textColor: "white"
+})
+
+export const DarkThemeContextProvider = ({ children }) => {
+    const [darkTheme] = useState(null);
+
+    return (
+        <DarkThemeContext.Provider value={{darkTheme}}>
+            {children}
+        </DarkThemeContext.Provider>
+    )
+}
+
+export const LightThemeContext = createContext({
+    backgroundColor: "white",
+    forgroundColor: "gray",
+    textColor: "black"
+})
+
+export const LightThemeContextProvider = ({ children }) => {
+    const [lightTheme] = useState(null);
+    return (
+        <LightThemeContext.Provider value={{lightTheme}}>
+            { children }
+        </LightThemeContext.Provider>
+    )
+}
+
+export const EnglishLanguageContext = createContext({
+    title: {
+        firstPart: "Hello, I'm <span class='highlight'>Mehdi</span>, an ever-improving frontend developer specializing in React, dedicated to crafting responsive, user-friendly web applications."
+    },
+    body: "to be programed" 
+})
+
+export const EnglishLanguageContextProvider = ({ children }) => {
+    const [EnglishLanguage] = useState({
+        title: {
+            firstPart: "Hello, I'm <span class='highlight'>Mehdi</span>, an ever-improving frontend developer specializing in React, dedicated to crafting responsive, user-friendly web applications."
+        },
+        body: "to be programed" 
+    });
+
+    return (
+        <EnglishLanguageContext.Provider value={EnglishLanguage}>
+            {children}
+        </EnglishLanguageContext.Provider>
     )
 }
