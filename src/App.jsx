b9ec930c597ebfe33d7fefd85e4dbae125017689
mehdi rezaei arshad <mehdirezaeiarshad@gmail.com>
2025-03-25 23:10:10 +0330

    finally finished the starter part

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "/src/css.files/App.css";
+import {motion} from "motion/react"
 import { ScreenSizeProvider, useScreenSize } from "./contexts";
 import { DarkThemeContext, DarkThemeContextProvider } from "./contexts";
 import { LightThemeContext, LightThemeContextProvider } from "./contexts";
-import {
-  EnglishLanguageContext,
-  EnglishLanguageContextProvider,
-} from "./contexts";
+import {EnglishLanguageContext,EnglishLanguageContextProvider} from "./contexts";
+import { ThemeContext, ThemeContextProvider } from "./contexts";
+import {dotsSvg} from './assets/title'
 
 const TypingAnimation = (fullText) => {
   const [displayText, setDisplayText] = useState("");
@@ -46,20 +46,15 @@ function TitlePart() {
           <button>plus</button>
         </div>
       ) : (
-        <div style={{ width: "100%" }}>
+        <div id="title-name" style={{ width: "100%" }}>
           {/* this for laptop */}
-          <div
-            style={{ textAlign: "left" }}
-            className="mt-auto mb-auto ms-auto p-0"
-          >
-            <h2 style={{fontWeight: "bold"}} className="highlight p-0">Hello there, I'm </h2>
-            <div className="clo-8">
-              <h1 className="name p-0 fade-in ms-auto col-12">Mehdi Rezaei</h1>
+          <div style={{ textAlign: "left" }} className="mt-auto mb-auto offset-md-2 p-0">
+            <h2 style={{fontWeight: "bold"}} className="highlight mb-0 p-0 h2-clamp">Hello there, I'm -</h2>
+            {dotsSvg}
+            <div className="col-8 p-0 mt-0">
+              <h1 className="name h1-clamp p-0 m-0 fade-in ms-auto col-12">Mehdi Rezaei</h1>
             </div>
-            
-            <p className="col-3 p-0 ms-auto">{title.firstPart}</p>
-
-            
+            <h4 className="col-4 p-0 me-auto">{title.firstPart}</h4>
           </div>
         </div>
       )}
@@ -68,24 +63,62 @@ function TitlePart() {
 }
 
 function NavBar() {
-    const sun = document.getElementById("Capa_1")
-    const moon = document.getElementById("Capa_2")  
+    const root = document.documentElement;
+    const sunRef = useRef(null)
+    const moonRef = useRef(null) 
+    const {theme, setTheme} = useContext(ThemeContext)
+
+    useEffect(() => {
+
+    }, [theme])
     
   const changeTheme = () => { 
-
-    sun.classList.add("rotate");
+    if (theme === "dark") {
+      sunRef.current?.classList.add("fadeout")
+      setTheme("light")
+    }
+    else {
+      moonRef.current?.classList.add("fadeout")
+      setTheme("dark")
+    }
   }
 
-  const handleAnimationEnd = () => {
-     console.log("hello")
+  const handleAnimationEnd = (element) => {
+    if (element === "sun" && theme === "light") {
+      sunRef.current?.classList.remove("fadeout")
+      sunRef.current?.classList.add("hidden")
+      root.style.setProperty('--background-color', 'white')
+      root.style.setProperty('--forground-color', 'gray')
+      root.style.setProperty('--color', 'black')
+      root.style.setProperty('--highlight-color', '#00cee4')
+      moonRef.current?.classList.remove("hidden")
+      moonRef.current?.classList.add("fadein")
+    }
+    else if (element === "sun" && theme !== "light") {
+      sunRef.current?.classList.remove("fadein")
+      sunRef.current?.classList.remove("fadeout")
+    }
+    else if (element === "moon" && theme === "dark") {
+      moonRef.current?.classList.remove("fadeout")
+      moonRef.current?.classList.add("hidden");
+      root.style.setProperty('--background-color', '#000000')
+      root.style.setProperty('--forground-color', 'gray')
+      root.style.setProperty('--color', 'white')
+      root.style.setProperty('--highlight-color', '#00cee4')
+      sunRef.current?.classList.remove("hidden");
+      sunRef.current?.classList.add("fadein")
+    }
+    else if (element === "moon" && theme !== "dark") {
+      moonRef.current?.classList.remove("fadein")
+      moonRef.current?.classList.remove("fadeout")
+    }
   }
-
   return <div id="navbar" className="d-flex mt-2 justify-content-between">
     <h1 className="col-3 text-center" id="header-name">arshad</h1>
     <div className="col-3">
       <button onClick={() => changeTheme()} className="d-block ms-auto h-100">
-      <svg onAnimationEnd={handleAnimationEnd} fill="#ffffff" height="800px" width="800px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
-	 viewBox="0 0 207.628 207.628" xml:space="preserve">
+      <svg ref={sunRef} onAnimationEnd={() => handleAnimationEnd("sun")} fill="#ffffff" height="800px" width="800px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" 
+	 viewBox="0 0 207.628 207.628" xmlSpace="preserve">
 <circle cx="103.814" cy="103.814" r="45.868"/>
 <path d="M103.814,157.183c-29.427,0-53.368-23.941-53.368-53.368s23.941-53.368,53.368-53.368s53.368,23.941,53.368,53.368
 	S133.241,157.183,103.814,157.183z M103.814,65.446c-21.156,0-38.368,17.212-38.368,38.368s17.212,38.368,38.368,38.368
@@ -111,10 +144,12 @@ function NavBar() {
 	c2.929-2.929,7.678-2.93,10.606,0l17.243,17.242c2.929,2.929,2.929,7.677,0,10.606C56.791,59.72,54.872,60.452,52.952,60.452z"/>
 </svg>
 
-<svg fill="#ffffff" id="Capa_2" className="hidden" width="800px" height="800px" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="M283.211 512c78.962 0 151.079-35.925 198.857-94.792 7.068-8.708-.639-21.43-11.562-19.35-124.203 23.654-238.262-71.576-238.262-196.954 0-72.222 38.662-138.635 101.498-174.394 9.686-5.512 7.25-20.197-3.756-22.23A258.156 258.156 0 0 0 283.211 0c-141.309 0-256 114.511-256 256 0 141.309 114.511 256 256 256z"/></svg>
+<svg ref={moonRef} onAnimationEnd={() => handleAnimationEnd("moon")} fill="#000000" id="Capa_2" className="hidden" width="800px" height="800px" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="M283.211 512c78.962 0 151.079-35.925 198.857-94.792 7.068-8.708-.639-21.43-11.562-19.35-124.203 23.654-238.262-71.576-238.262-196.954 0-72.222 38.662-138.635 101.498-174.394 9.686-5.512 7.25-20.197-3.756-22.23A258.156 258.156 0 0 0 283.211 0c-141.309 0-256 114.511-256 256 0 141.309 114.511 256 256 256z"/></svg>
       </button>
     </div>
-    <button className="col-1">lang</button>
+    <button className="col-1">
+    lan
+  </button>
     <button className="col-1">home</button>
     <button className="col-1">projects</button>
     <div className="col-2">
@@ -148,6 +183,7 @@ function DashBoard() {
 function App() {
   return (
     <>
+    <ThemeContextProvider>
       <EnglishLanguageContextProvider>
         <LightThemeContextProvider>
           <DarkThemeContextProvider>
@@ -156,8 +192,10 @@ function App() {
             </ScreenSizeProvider>
           </DarkThemeContextProvider>
         </LightThemeContextProvider>
-      </EnglishLanguageContextProvider>
+      </EnglishLanguageContextProvider>      
+    </ThemeContextProvider>
     </>
   );
 }
+
 export default App;
